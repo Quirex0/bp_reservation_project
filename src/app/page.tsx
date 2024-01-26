4524689c5fc9057d3f0fa0a906f54ed5157cd01e
Quirex0 <mirekcipra@seznam.cz>
2024-01-26 14:30:56 +0100

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import Image from "next/image";
-import styles from "../styles/page.module.css";
 import { Metadata } from 'next'
 import Navbar from '../components/Navbar';
+import Footer from "@/components/Footer";
  
 export const metadata: Metadata = {
   title: 'My Page Title',
 }
 export default function Home() {
   return (
-    
+    <div>
       <Navbar />
+      <main>
+        {/* Obsah stránky */}
+        <h1>Vítejte na stránce</h1>
+        <p>Toto je obsah stránky.</p>
+      </main>
+      <Footer />
+    </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from "../styles/Footer.module.css";
+
+const Footer = () => {
+    return (
+      <div className={styles.footer}>
+      <p>
+        Copyright &copy; Jitka Moučková <br />
+      </p>
+    </div>
+  );
+  };
+  export default Footer;
