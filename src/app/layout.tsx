09fd5b3cead1fa53b32898e3c37c0e4c725183f9
Quirex0 <mirekcipra@seznam.cz>
2024-03-23 19:33:30 +0100

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,17 +8,20 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <html>
       <Head>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title></title>
-        <meta name="description"  />
+        <meta name="description" />
         <link rel="shortcut icon" href="./favicon.svg" type="image/svg+xml" />
       </Head>
-      {children}
-    </>
+      <body>
+        {children}
+      </body>
+
+    </html>
   );
 };
 
